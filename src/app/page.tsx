0e452aceb02acee420955e9b3a7a21fbab44d030
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-22 03:22:10 -0800

    change code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import MainLayout from "@/components/MainLayout";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
@@ -6,7 +8,7 @@ const HomePage: React.FC = () => {
   return (
     <MainLayout>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold mb-6">حالت تم: روشن / تاریک</h1>
+        <h1 className="text-4xl font-bold mb-6">Welcome to Costly</h1>
         <ThemeSwitcher />
       </div>
     </MainLayout>
