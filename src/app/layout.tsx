ff1e51d88f09a576f6f2869c88d7676d0f9f810f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-22 03:22:01 -0800

    change code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
+"use client";
+
 import "./globals.css"; // استایل‌های عمومی
 import { ThemeProvider } from "@/context/ThemeContext";
-
-export const metadata = {
-  title: "Context API Theme Example",
-};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className="min-h-screen">
-        {/* ارائه ThemeProvider به کل اپلیکیشن */}
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
