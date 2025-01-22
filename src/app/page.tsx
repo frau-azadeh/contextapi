d6cbcb617c2b6f92bed687b12e97b7bd2594351d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-22 05:40:57 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import MainLayout from "@/components/MainLayout";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
+import ExpenseForm from "@/components/ExpenseForm";
+import ExpenseList from "@/components/ExpenseList";
 
 const HomePage: React.FC = () => {
   return (
@@ -10,6 +12,8 @@ const HomePage: React.FC = () => {
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold mb-6">Welcome to Costly</h1>
         <ThemeSwitcher />
+        <ExpenseForm/>
+        <ExpenseList/>
       </div>
     </MainLayout>
   );
