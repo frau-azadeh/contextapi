d5ad1ed53c66fc259daeb930d825b2890fe7016e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-22 03:22:27 -0800

    change code

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,16 +1,18 @@
-"use client"
 import React from "react";
 import { useTheme } from "@/context/ThemeContext";
-import { FiSun, FiMoon } from "react-icons/fi"; // آیکن‌ها از React Icons
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const ThemeSwitcher: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
+
+  // اگر theme هنوز مقداردهی نشده است، چیزی رندر نکنید
+  if (!theme) return null;
 
   return (
     <button
       onClick={toggleTheme}
       className="p-2 rounded-full bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 fixed top-4 right-4 shadow-lg transition duration-300"
-      aria-label="Toggle Theme"
+      aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
     >
       {theme === "dark" ? (
         <FiSun className="text-yellow-400 w-6 h-6" />
@@ -22,4 +24,3 @@ const ThemeSwitcher: React.FC = () => {
 };
 
 export default ThemeSwitcher;
-
