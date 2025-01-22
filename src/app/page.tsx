b1238b279605b7e3369eaa941a9bcd2b41859d7d
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-22 01:20:49 -0800

    change code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,16 @@
-'use client'
 import React from "react";
-import DoctorCard from "@/components/DoctorCard";
-import { useDoctors } from "@/hooks/useDoctors";
+import MainLayout from "@/components/MainLayout";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 
-const DoctorsPage = () => {
-  const { doctors, loading, error } = useDoctors(); // استفاده از هوک سفارشی
-
-  if (loading) {
-    return <p className="text-center mt-10">در حال بارگذاری...</p>;
-  }
-
-  if (error) {
-    return <p className="text-center mt-10 text-red-500">{error}</p>;
-  }
-
+const HomePage: React.FC = () => {
   return (
-    <div className="container mx-auto px-4 py-6">
-      <h1 className="text-2xl font-bold text-center mb-6">لیست پزشکان</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {doctors.map((doctor) => (
-          <DoctorCard key={doctor.id} doctor={doctor} />
-        ))}
+    <MainLayout>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-6">حالت تم: روشن / تاریک</h1>
+        <ThemeSwitcher />
       </div>
-    </div>
+    </MainLayout>
   );
 };
 
-export default DoctorsPage;
+export default HomePage;
